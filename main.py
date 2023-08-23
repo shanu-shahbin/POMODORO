7e6a4b45b60b7f2ba98de5f07da61329c9315b96
shanu shahbin <shanushahbin11@gmail.com>
2023-08-23 11:33:17 +0530

    buttons and labels are created

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,23 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+title_label = Label(text="TIMER", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35))
+title_label.grid(column=1, row=0)
+canvas = Canvas(width=200, height=223, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_mark = Label(text="✓", fg=GREEN, bg=YELLOW)
+check_mark.grid(column=1, row=3)
+window.mainloop()
